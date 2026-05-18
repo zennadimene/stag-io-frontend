5b5e3a4bea47513fdb6057396fa6679f7e19fd32
zennadimene <zennadimene@github.com>
2026-05-18 18:05:03 +0100

    Hardcode API URL for production

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
-//const API_URL = 'http://localhost:5000/api';
+//const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_URL = 'https://stag-io-backend.onrender.com/api';
+//const API_URL = 'http://localhost:5000/api'; original
 
 const api = axios.create({
     baseURL: API_URL,
